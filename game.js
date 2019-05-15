c6760e05929905a37c58c40d3c4361313cacbb9c
magda <magda17095@wp.pl>
2019-05-15 13:54:39 +0200

    add winner

--- game.js
+++ game.js
@@ -21,23 +21,33 @@ const gameSummary = {
     this.style.boxShadow = '0 0 0 4px red';
    }
    
-
+ 
    
    function aiChoice() {
     return hands[Math.floor(Math.random() * 3)].dataset.option;
    }
    
+   function checkResult(player, ai) {
+    if (player === ai) {
+     return 'remis';
+    } else if ((player === "papier" && ai === "kamień") || (player === "kamień" && ai === "nożyczki") || (player === "nożyczki" && ai === "papier")) {
+     return 'wygrałeś';
+    } else { return 'przegrałeś'; }
+   }
    
+
    function startGame() {
     if (!game.playerHand) {
-     return alert("wybierze dłoń!!!!")
+     return alert("wybierz dłoń!!!!");
     }
-    game.aiHand = aiChoice()
-   
+    game.aiHand = aiChoice();
+    const gameResult = checkResult(game.playerHand, game.aiHand);
+    console.log(gameResult);
    }
    
    hands.forEach(hand => hand.addEventListener('click', handSelection))
    
    document.querySelector('.start').addEventListener('click', startGame)
+   
    
    
